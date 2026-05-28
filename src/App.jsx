d1b8eebada450b5938157fc5d83d64e88fe671ce
nairx <nairxprv@gmail.com>
2026-05-28 15:42:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import RootLayout from "./components/RootLayout";
+import Cart from "./components/Cart";
+import Order from "./components/Order";
+import AdminLayout from "./components/AdminLayout";
+import Products from "./components/Products";
+import Orders from "./components/Orders";
+import Users from "./components/Users";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ErrorPageGlobal from "./components/ErrorPageGlobal";
+import ErrorPage from "./components/ErrorPage";
+
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      errorElement: <ErrorPageGlobal />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "cart", element: <Cart />, errorElement: <ErrorPage /> },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
