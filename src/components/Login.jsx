92ebb7573f90c63763232b9e292bb1d22e425883
nairx <nairxprv@gmail.com>
2026-06-03 15:36:08 +0530

    updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { login } from "../features/userSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
 export default function Login() {
   const [user, setUser] = useState({});
+  const [users, setUsers] = useState([]);
   const [message, setMessage] = useState();
-  const currentUser = useSelector(state => state.user.isLoggedIn);
+  const currentUser = useSelector((state) => state.user.isLoggedIn);
   const Dispatch = useDispatch();
+  const API = import.meta.env.VITE_API_URL + "/users";
   const Navigate = useNavigate();
   const handleLogin = () => {
-    const flag = Dispatch(login(user));
-    if (currentUser) Navigate("/");
+    // const flag = Dispatch(login(user));
+    const flag = users.find(elem=>elem.email === user.email && elem.password === user.password)
+    if (flag) Navigate("/");
     else setMessage("User not found");
   };
+
+  useEffect(() => {
+    axios
+      .get(API)
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       Login Form
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import axios from "axios";
 import { register } from "../features/userSlice";
 
 export default function Register() {
   const [user, setUser] = useState({});
   const Dispatch = useDispatch();
   const Navigate = useNavigate();
-  const handleSubmit = () => {
-    Dispatch(register({...user,role:"user"}));
+  const API = import.meta.env.VITE_API_URL + "/users";
+  const handleSubmit = async () => {
+    // Dispatch(register({...user,role:"user"}));
+    await axios.post(API, user);
     Navigate("/login");
   };
   return (
